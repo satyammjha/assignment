3599a4274a660fcd5f7daf02f954f8c682653e12
satyammjha <satyammjha0@gmail.com>
2025-10-17 11:33:58 +0530

    frontend setup

--- models/resumeReview.models.ts
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-
-
-const resumeReviewSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-})
--- utils/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const connectToDb = async () => {
-  await mongoose.connect(process.env.MONGO_URI);
-  console.log("Connected to DB");
-};
-
-export default connectToDb;
